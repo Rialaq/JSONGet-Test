e4ae2231e223c428c5743a3ee5745fb87937e2ca
Ricardo Almonte <51594905+Rialaq@users.noreply.github.com>
2020-01-03 16:07:15 -0400

    Subida de Ordenes

---
+++ DotnetAPI Hello World/Controllers/TrueController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DotnetAPI_Hello_World.Models;
+
+namespace DotnetAPI_Hello_World.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrueController : Controller
+    {
+        [HttpGet]
+        public JsonResult Test()
+        {
+            var name = new List<names>
+            {
+                new names {username = "rialaq98",    password = "1415168rd" },
+                new names {username = "miguel_rojas98",     password = "rojasPower" },
+                new names {username = "starling",   password = "MLebron" },
+                new names {username = "pacoman",     password = "reylore" }
+
+            };
+            return Json(name);
+        }
+    }
+}
